6c6a63c4c1d1caaa01589d0e7be7555b0dfa0da8
Eduardo-fix <63111989+Eduardo-fix@users.noreply.github.com>
2021-08-22 20:33:18 -0300

    Add files via upload

---
+++ Aula02.java
@@ -0,0 +1,11 @@
+// 2010552 - Eduardo Max dos Reis Silva
+
+package POO_2021_02;
+
+public class Aula02 {
+
+        public static void main(String[] args) {
+            
+            System.out.println("Hello World!!!");
+        }
+}
